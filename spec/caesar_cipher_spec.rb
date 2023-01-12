978ae5e3c2ff110fedb767d0c38c8507a413a801
katie <katiepb525@gmail.com>
2023-01-12 15:36:30 -0600

    write second test

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -4,4 +4,8 @@ describe "#caesar_cipher" do
   it "returns a ciphered single letter word" do
     expect(caesar_cipher('Hello', -3)).to eql('Ebiil')
   end
+
+  it "returns a ciphered sentence" do 
+    expect(caesar_cipher('Hello there', -3)).to eql('Ebiil qebob')
+  end
 end
